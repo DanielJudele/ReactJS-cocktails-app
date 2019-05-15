8cb7f980e6ae58ba4ccb1f36942b8a64756d2782
Judele <daniel.judele@centric.eu>
2019-05-15 09:33:29 +0300

    move navbar to molecules

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/organisms/header';
-import NavBar from './components/molecules/navbar';
+import NavBar from './components/organisms/navbar';
 
 function App() {
   return (
